14eeff41cfd53825d2dc5f2b5deb6a12877c9161
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-05-13 00:38:27 +0900

    Initial commit

---
+++ test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10bb503b-3db2-4a55-8d85-233270a0bd9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PriorityQueueEmptyTest()
+        {
+            PriorityQueue.PriorityQueue<int> queue = new PriorityQueue.PriorityQueue<int>();
+            Assert.IsTrue(queue.Empty);
+        }
+        [TestMethod]
+        public void PriorityQueueTest()
+        {
+            PriorityQueue.PriorityQueue<int> queue = new PriorityQueue.PriorityQueue<int>();
+            var ans = new int[] { 1, 2, 3, 4, 6, 8, 9 };
+            queue.Enqueue(new int[] { 1, 4, 2, 3, 9, 8, 6 });
+
+            var i = 0;
+            while (!queue.Empty)
+            {
+                Assert.AreEqual(ans[i++], queue.Dequeue());
+            }
+        }
+    }
+}
